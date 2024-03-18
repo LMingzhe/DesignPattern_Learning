e4ed18cdad9d9c607c52170d256f584834b91a12
Liu Mingzhe <15768835224@163.com>
2024-03-18 09:38:40 +0800

    update build.cpp

--- 创建型模式/建造者模式/builder.cpp
+++ 创建型模式/建造者模式/builder.cpp
@@ -3,17 +3,24 @@
 
 using namespace std;
 
+// 自行车产品
 class Bicycle
 {
 public:
-    void setFrame(std::string frame)
+    void setFrame(const std::string& frame)
     {
-        frame = frame;
+        this->frame = frame;
     }
 
-    void setTires(std::string tires)
+    void setTires(const std::string& tires)
     {
-        tires = tires;
+        this->tires = tires;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Bicycle& bicycle)
+    {
+        os << bicycle.frame << " " << bicycle.tires;
+        return os;
     }
 
 private:
@@ -21,63 +28,108 @@ private:
     std::string tires;
 };
 
+// 自行车抽象建造者
 class BicycleBuilder
 {
 public:
     virtual void buildFrame() = 0;
     virtual void buildTires() = 0;
-    virtual Bicycle* getresult() = 0;
+    virtual Bicycle getresult() = 0;
 };
 
+// 山地车具体建造者
 class MountainBuilder : public BicycleBuilder
 {
 public:
     void buildFrame() override
     {
-        mountainBicycle->setFrame("Aluminum Frame");
+        mountainBicycle.setFrame("Aluminum Frame");
     }
 
     void buildTires() override
     {
-        mountainBicycle->setTires("Knobby Tires");
+        mountainBicycle.setTires("Knobby Tires");
     }
 
-    Bicycle* getresult() override
+    Bicycle getresult() override
     {
         return mountainBicycle;
     }
 private:
-    Bicycle* mountainBicycle;
+    Bicycle mountainBicycle;
 };
 
+// 公路车具体建造者
 class RoadBuilder : public BicycleBuilder
 {
 public:
     void buildFrame() override
     {
-        RoadBicycle->setFrame("Carbon Frame");
+        RoadBicycle.setFrame("Carbon Frame");
     }
 
     void buildTires() override
     {
-        RoadBicycle->setTires("Slim Tries");
+        RoadBicycle.setTires("Slim Tires");
     }
 
-    Bicycle* getresult() override
+    Bicycle getresult() override
     {
         return RoadBicycle;
     }
 private:
-    Bicycle* RoadBicycle;
+    Bicycle RoadBicycle;
 };
 
+// 自行车建造指导者
 class BicycleDirector
 {
 public:
-    Bicycle* construct(BicycleBuilder* builder)
+    Bicycle construct(BicycleBuilder& builder)
     {
-        builder->buildFrame();
-        builder->buildTires();
-        return builder->getresult();
+        builder.buildFrame();
+        builder.buildTires();
+        return builder.getresult();
     }
 };
+
+int main(int argc, char const *argv[])
+{
+    // 指导者对象，负责指导产品生成
+    BicycleDirector director;
+    int cnt = 0;
+    std::string type;
+
+    std::cin >> cnt;
+    if (cnt < 1 || cnt > 100)
+    {
+        std::cout << "输入有误！" << std::endl;
+        return 0;
+    }
+
+    while (cnt--)
+    {
+        BicycleBuilder* builder;
+        std::cin >> type;
+        // 根据不同需求创建不同的具体建造者
+        if (type == "mountain")
+        {
+            builder = new MountainBuilder();
+        }
+        else if (type == "road")
+        {
+            builder = new RoadBuilder();
+        }
+        else
+        {
+            std::cout << "输入有误！" << std::endl;
+            cnt += 1;
+            continue;
+        }
+        Bicycle bike = director.construct(*builder);
+        std::cout << bike << std::endl;
+        delete builder;
+    }
+    return 0;
+}
+
