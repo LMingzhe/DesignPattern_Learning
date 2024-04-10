d2227bdbcfe89345c90697f71ccb0a29e4f1b77a
Liu Mingzhe <15768835224@163.com>
2024-04-10 20:50:28 +0800

    add strategy.cpp

---
+++ 行为型模式/策略模式/strategy.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::cin;
+using std::cerr;
+
+// 抽象策略类
+class Strategy
+{
+public:
+    virtual int compute(int price) = 0;
+};
+
+// 具体策略类
+class Discount_A : public Strategy
+{
+public:
+    int compute(int price) override
+    {
+        return price * 0.9;
+    }
+};
+
+// 具体策略类
+class Discount_B : public Strategy
+{
+public:
+    int compute(int price) override
+    {
+        if (price >= 300) { return price - 40; }
+        else if (price >= 200) { return price - 25; }
+        else if (price >= 150) { return price - 15; }
+        else if (price >= 100) { return price - 5; }
+        else { return price; }
+    }
+};
+
+// 上下文类
+class Context
+{
+private:
+    Strategy* discount;
+
+public:
+    Context(Strategy* discount) : discount(discount) {}
+
+    void contextInterface(int price)
+    {
+        int priceDiscounted = discount->compute(price);
+        cout << priceDiscounted << endl;
+    }
+
+    ~Context() { delete discount; }
+};
+
+int main(int argc, char const *argv[])
+{
+    int cnt;
+    cin >> cnt;
+    if (cnt < 1 || cnt > 20) 
+    {
+        cerr << "输入有误！" << endl;
+        return 0;
+    }
+    while (cnt--)
+    {
+        int price, discountType;
+        cin >> price >> discountType;
+        if (discountType == 1)
+        {
+            Context* context = new Context(new Discount_A);
+            context->contextInterface(price);
+            delete context;
+        }
+        else 
+        {
+            Context* context = new Context(new Discount_B);
+            context->contextInterface(price);
+            delete context;
+        }
+    }
+    return 0;
+}
